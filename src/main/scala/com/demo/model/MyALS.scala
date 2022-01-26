a177982e63faec5578f39d141e42e54ed09d930d
PinskyRobin <xu568059888@163.com>
2022-01-26 16:18:11 +0800

    optimize evaluation function

--- src/main/scala/com/demo/model/MyALS.scala
+++ src/main/scala/com/demo/model/MyALS.scala
@@ -19,29 +19,15 @@ import scala.util.Random
  * @param seed       随机初始化矩阵的种子
  */
 
-class MyALS(private val rank: Int = 20,
+class MyALS(private val rank: Int = 30,
 			private val iterations: Int = 10,
 			private val lambda: Double = 0.1,
 			private val blocks: Int = -1,
 			private val seed: Long = System.nanoTime()) {
 
   private var model: Option[MatrixFactorizationModel] = None
-  //  private var bestModel: Option[MatrixFactorizationModel] = None
 
-  /** *
-   * 输出有含义的推荐列表
-   *
-   * @param recs   推荐列表
-   * @param mapper 物品 id 到名字的映射
-   */
-  def _printRecs(recs: RDD[(Int, Array[Int])], mapper: mutable.Map[Int, String]): Unit =
-	recs.foreach {
-	  t =>
-		print(s"\r\n=== Recommend for USER${t._1} ===\r\n")
-		for (i <- t._2.indices) {
-		  print(s"ITEM$i: ${mapper(t._2(i))}\r\n")
-		}
-	}
+  def getModel: MatrixFactorizationModel = model.get
 
   def _getUserItemDict(data: RDD[Rating]): RDD[(Int, Iterable[Int])] =
 	data.map(
@@ -61,23 +47,24 @@ class MyALS(private val rank: Int = 20,
 	)
 
 	val ratings = predictionRate.join(testRate).values
+
 	math.sqrt(ratings.map(x => (x._1 - x._2) * (x._1 - x._2)).reduce(_ + _) / test.count())
   }
 
-  def _calPrecision(sc: SparkContext, len: Long, dict: RDD[(Seq[Int], Seq[Int])]): Double = {
+  def _calPrecision(sc: SparkContext, len: Long, dict: RDD[(Set[Int], Set[Int])]): Double = {
 	val acc = sc.doubleAccumulator("precisionAccumulator")
 	dict.foreach(
 	  t =>
-		acc.add(t._2.intersect(t._1).length.toDouble / t._2.length)
+		acc.add(t._2.intersect(t._1).size.toDouble / t._2.size)
 	)
 	acc.sum / len
   }
 
-  def _calRecall(sc: SparkContext, len: Long, dict: RDD[(Seq[Int], Seq[Int])]): Double = {
+  def _calRecall(sc: SparkContext, len: Long, dict: RDD[(Set[Int], Set[Int])]): Double = {
 	val acc = sc.doubleAccumulator("recallAccumulator")
 	dict.foreach(
 	  t =>
-		acc.add(t._2.intersect(t._1).length.toDouble / t._2.length)
+		acc.add(t._2.intersect(t._1).size.toDouble / t._1.size)
 	)
 	acc.sum / len
   }
@@ -99,7 +86,17 @@ class MyALS(private val rank: Int = 20,
 		recItemAcc.add(t._2.toSet)
 	)
 
-	recItemAcc.value.toArray.distinct.length.toDouble / itemAcc.value.toArray.distinct.length
+	var itemSet: mutable.Set[Int] = mutable.Set()
+	var recItemSet: mutable.Set[Int] = mutable.Set()
+
+	recItemAcc.value.forEach(
+	  t => recItemSet = recItemSet ++ t
+	)
+
+	itemAcc.value.forEach(
+	  t => itemSet = itemSet ++ t
+	)
+	recItemSet.size.toDouble / itemSet.size
   }
 
   def _calAUC(test: RDD[Rating], _allItemIDs: Broadcast[Array[Int]]): Double = {
@@ -164,64 +161,16 @@ class MyALS(private val rank: Int = 20,
 	println("Training completed!")
   }
 
-
-  //  /** *
-  //   * 自动寻找一定范围内的最优参数组合
-  //   *
-  //   * @param ratings 评分 RDD 数组
-  //   */
-  //  def autoFit(ratings: RDD[Rating]): Unit = {
-  //	println("This may process for a long time, please wait patiently.")
-  //	println("Start exploring best parameters...")
-  //
-  //	val randomSeq = ratings.randomSplit(Array(0.6, 0.4))
-  //	val training = randomSeq(0)
-  //	val validation = randomSeq(1)
-  //
-  //	val _ranks = List(5, 30)
-  //	val _iterations = List(5, 20)
-  //	val _lambdas = List(0.01, 3.00)
-  //	var bestRank = 0
-  //	var bestIterations = 0
-  //	var bestLambda = 0.0
-  //	var bestScore = 100.0
-  //
-  //	for (_rank <- _ranks; _iteration <- _iterations; _lambda <- _lambdas) {
-  //	  model = Option(ALS.train(training, _rank, _iteration, _lambda))
-  //	  val score = _calRMSE(validation, predict(80))
-  //	  if (score < bestScore) {
-  //		bestScore = score
-  //		bestRank = _rank
-  //		bestIterations = _iteration
-  //		bestLambda = _lambda
-  //		bestModel = model
-  //		println("**Parameter updated!**")
-  //		println("Optimized Score: " + bestScore)
-  //		println("Optimized Rank: " + bestRank)
-  //		println("Optimized Iterations: " + bestIterations)
-  //		println("Optimized Lambda: " + bestLambda)
-  //	  }
-  //	}
-  //
-  //	println("BestScore: " + bestScore)
-  //	println("BestRank: " + bestRank)
-  //	println("BestIterations: " + bestIterations)
-  //	println("BestLambda: " + bestLambda)
-  //
-  //	ratings.unpersist()
-  //  }
-
   /** *
-   * 返回推荐列表，格式为
+   * 预测，返回推荐列表，格式为
    * [
    * [UserId, [ItemIdList]\],
    * ...
    * ]
    *
    * @param recNum 推荐的数量
-   * @param trans  是否返回物品 id 对应的名字
    */
-  def predict(recNum: Int, trans: Boolean = false, mapper: mutable.Map[Int, String] = null): RDD[(Int, Iterable[Int])] = {
+  def predict(recNum: Int): RDD[(Int, Iterable[Int])] = {
 
 	var recs: RDD[(Int, Array[Rating])] = null
 
@@ -238,7 +187,6 @@ class MyALS(private val rank: Int = 20,
 	  rec =>
 		(rec._1, rec._2.map(t => t.product).toIterable)
 	)
-	//TODO:trans部分
 	println("Predicting completed!")
 	res
   }
@@ -246,33 +194,43 @@ class MyALS(private val rank: Int = 20,
   /** *
    * 比对测试集和推荐结果的差异，给出详细数据
    *
+   * @param sc             SparkContext 实例
+   * @param train          训练集数组
    * @param test           测试集数组
    * @param predictionDict 推荐的用户 - [物品]字典
-   * @return 包含 precision、recall 和 coverage 的 Map
+   * @param _allItemIDs    所有物品的 ID 列表
+   * @return 包含 RMSE、AUC 和 coverage 的 Map
    */
-  def score(sc: SparkContext, train: RDD[Rating],
+  def score(sc: SparkContext,
+			train: RDD[Rating],
 			test: RDD[Rating],
 			predictionDict: RDD[(Int, Iterable[Int])],
 			_allItemIDs: Array[Int]): mutable.Map[String, Double] = {
+
 	val resMap: mutable.Map[String, Double] = mutable.Map()
 
 	// transform data
-	val trainDict = _getUserItemDict(train)
-	val testDict = _getUserItemDict(test)
+	val trainDict = _getUserItemDict(train).cache()
+	val testDict = _getUserItemDict(test).cache()
 	val allItemIDs = sc.broadcast(_allItemIDs)
 
-	val dict = testDict.join(predictionDict.sortByKey()).values.map(
+	val dict = testDict.join(predictionDict).sortByKey().values.map(
 	  t =>
-		(t._1.toSeq, t._2.toSeq)
-	)
+		(t._1.toSet, t._2.toSet)
+	).cache()
+
 	val testLength = test.count()
 	val predLength = predictionDict.count()
 
 	resMap += "RMSE" -> _calRMSE(test, predictionDict)
-	resMap += "precision" -> _calPrecision(sc, predLength, dict)
-	resMap += "recall" -> _calRecall(sc, testLength, dict)
+//	resMap += "precision" -> _calPrecision(sc, predLength, dict)
+//	resMap += "recall" -> _calRecall(sc, testLength, dict)
 	resMap += "coverage" -> _calCoverage(sc, trainDict, testDict, predictionDict)
 	resMap += "AUC" -> _calAUC(test, allItemIDs)
+
+	trainDict.unpersist()
+	testDict.unpersist()
+	dict.unpersist()
 
 	resMap
   }
