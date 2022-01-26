af43eba1f144307ad35017369a51e0bcbf9ff7b2
PinskyRobin <xu568059888@163.com>
2022-01-26 16:19:22 +0800

    add @After operation and many tests for variable hyperparameter combinations

--- src/test/scala/com/demo/MyALS_test.scala
+++ src/test/scala/com/demo/MyALS_test.scala
@@ -39,16 +39,16 @@ class MyALS_test {
 	sc = new SparkContext(new SparkConf().setAppName("ALS").setMaster("local[*]"))
 
 	// read data from HDFS and delete header line
-	val rawRatings = sc.textFile("hdfs://localhost:9000/input/ml_data_small/ratings.csv")
+	val rawRatings = sc.textFile("hdfs://localhost:9000/input/ml_data/ratings.csv")
 	  .zipWithIndex().filter(_._2 >= 1).keys
 
-	val rawMovies = sc.textFile("hdfs://localhost:9000/input/ml_data_small/movies.csv")
+	val rawMovies = sc.textFile("hdfs://localhost:9000/input/ml_data/movies.csv")
 	  .zipWithIndex().filter(_._2 >= 1).keys
 
 	// split raw dataset into training set and testing set and transform to RDD[Rating]
-	val randomSeq = rawRatings.randomSplit(Array(0.6, 0.4))
+	val randomSeq = rawRatings.randomSplit(Array(0.7, 0.3))
 	train = _rddStr2rddRatings(randomSeq(0))
-//	train = _rddStr2rddRatings(sc.parallelize(rawRatings.randomSplit(Array(1,0))(0).collect(), numSlices = 100))
+	//	train = _rddStr2rddRatings(sc.parallelize(rawRatings.randomSplit(Array(1,0))(0).collect(), numSlices = 100))
 	test = _rddStr2rddRatings(randomSeq(1))
 
 	mapper = _id2Name(rawMovies)
@@ -62,11 +62,198 @@ class MyALS_test {
 
   @Test
   def test01(): Unit = {
-	val myALS = new MyALS(30, 10, 0.1)
+	val myALS = new MyALS(30, 20, 0.1)
 	myALS.fit(train)
-//	myALS.autoFit(train)
+	//	myALS.autoFit(train)
 
 	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test02(): Unit = {
+	val myALS = new MyALS(50, 20, 0.1)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test03(): Unit = {
+	val myALS = new MyALS(30, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test04(): Unit = {
+	val myALS = new MyALS(50, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test05(): Unit = {
+	val myALS = new MyALS(50, 20, 1)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test06(): Unit = {
+	val myALS = new MyALS(30, 20, 1)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test07(): Unit = {
+	val myALS = new MyALS(50, 20, 0.001)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test08(): Unit = {
+	val myALS = new MyALS(30, 20, 0.001)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(80)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test09(): Unit = {
+	val myALS = new MyALS(50, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(120)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test10(): Unit = {
+	val myALS = new MyALS(30, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(120)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test11(): Unit = {
+	val myALS = new MyALS(50, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(50)
+
+	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
+
+	score.foreach(
+	  t =>
+		printf("%s: %.4f\n", t._1, t._2)
+	)
+
+  }
+
+  @Test
+  def test12(): Unit = {
+	val myALS = new MyALS(30, 20, 0.01)
+	myALS.fit(train)
+	//	myALS.autoFit(train)
+
+	val prediction = myALS.predict(50)
 
 	val score = myALS.score(sc, train, test, prediction, mapper.keys.toArray)
 
